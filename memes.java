ddd654ad3f7971d05eb3bd114d8387ac3eaf40f1
Cliona Rogers <Cliona Rogers>
2016-02-23 10:48:39 +0000

    test commit

--- memes.java
+++ memes.java
@@ -4,11 +4,10 @@ public static main(String Args[])
 <<<<<<< HEAD
 	Systems.out.println("Hi, I'm test 2 lol")
 =======
-	Systems.out.println("Hi, I'm  Lolence");
+	Systems.out.println("Hi, I'm  Cliona");
 =======
 >>>>>>> 391509864ef154884e133c09f46a76c879e25e89
 	
-	Systems.out.println("This is not updated");
 >>>>>>> c461b32ae026ec32fdd5d90e6f288b38dff529e4
 
 	System.out.println("i like memes");
