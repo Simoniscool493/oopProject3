f8f6e89301f206c1e862125fa8527e3654539c9c
KaiChan <Kaichan39@outlook.com>
2016-03-27 16:27:28 +0100

    Buttons now work

--- src/bin/Frame.java
+++ src/bin/Frame.java
@@ -4,10 +4,17 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
+
 public class Frame extends JFrame{
 	
 	//Some fields
-	ButtonListener bListen = new ButtonListener();
+	//ButtonListener bListen = new ButtonListener();
+	//For sliders
+	static int sliderMin = 0;
+	static int sliderMax = 255;
+	static int sliderInit = 0;
+	static int componentWidth = 700;
+	static int componentHeight = 100;
 	
 	public Frame()
 	{
@@ -21,31 +28,67 @@ public class Frame extends JFrame{
 	public void init()
 	{
 		JPanel sketch = new JPanel();
-		processing.core.PApplet pixel = new Pixel();
+		Pixel pixel = new Pixel();
 		
 		//sketch.setLayout(new BorderLayout());
 		sketch.add(pixel);
 		sketch.setPreferredSize(new Dimension(500, 500));
 		
 		JPanel buttonsPanel = new JPanel();
+		buttonsPanel.setBackground(Color.WHITE);
 		//buttonsPanel.setLayout();
 		
 		/*2 buttons for now... just a test
 		Color palet and sliders should be a color of its own anyway*/
 		
 		//Draw
-		JButton Button = new JButton("Draw");
+		JButton drawButton = new JButton("Draw");
 		setPreferredSize(new Dimension(100,50));
-		buttonsPanel.add(Button);
+		buttonsPanel.add(drawButton);
 		
 		//Erase
-		Button = new JButton("Erase");
-		buttonsPanel.add(Button);
+		JButton eraseButton = new JButton("Erase");
+		buttonsPanel.add(eraseButton);
+		
+		//Button action listeners
+		drawButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Draw mode");
+				Pixel.mode = 1;
+			}
+		});
+		
+		eraseButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Erase mode");
+				Pixel.mode = 2;
+			}
+		});
 		
 		//Add things into frames
 		add(sketch, BorderLayout.WEST);
 		add(buttonsPanel);
 		pixel.init();
+		
+		//Adding sliders
+		JPanel sliderPanel = new JPanel();
+		
+	}
+	
+	public JSlider makeSlider(int n)
+	{
+		JSlider slider = new JSlider(JSlider.HORIZONTAL,sliderMin,sliderMax,sliderInit);
+		slider.setPreferredSize(new Dimension(componentWidth,componentHeight));
+		slider.setMajorTickSpacing(10);
+		slider.setMinorTickSpacing(1);
+		slider.setPaintTicks(true);
+		slider.setPaintLabels(true);
+		//slider.addChangeListener(new SliderListener(n));
+		return slider;
 	}
 	
 	public static void main(String[] args) {
