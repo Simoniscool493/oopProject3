e89c2ff27fcc6fbf6bf83b1891adc34290b7ad68
KaiChan <kaichan39@outlook.com>
2016-04-14 01:41:46 +0100

    Added a Menu Bar, we can add more to the menu bar

--- src/bin/Frame.java
+++ src/bin/Frame.java
@@ -8,6 +8,7 @@ import java.awt.GridLayout;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
@@ -17,6 +18,9 @@ import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.JTextField;
@@ -307,6 +311,24 @@ public class Frame extends JFrame { // implements ActionListener, KeyListener,
 				Pixel.backB = (int) source.getValue();
 			}
 		});
+		
+		//Add menu bar
+		JMenuBar menuBar = new JMenuBar();
+		JMenu menu = new JMenu("File");
+		menuBar.add(menu);
+		
+		JMenuItem menuItem1 = new JMenuItem("Save as image");
+		menuItem1.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				pixel.saveTrans();
+				
+			}
+		});
+		menu.add(menuItem1);
+		
+		this.setJMenuBar(menuBar);
 
 		// Adding Sliders into Toolbar
 		sliderPanel.add(redBackLabel);
